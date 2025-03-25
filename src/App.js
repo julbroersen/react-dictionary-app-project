7ccce67c80cf1a5ec3bd14435b7a51cdad1bff0f
julbroersen <julianabroersen2@gmail.com>
2025-03-25 14:58:51 +0100

    initial commit - created react app and added hero

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header img-fluid">
+        <h2>Which word are you curious about?</h2>
+      </header>
+      Hello World
+    </div>
+  );
+}
