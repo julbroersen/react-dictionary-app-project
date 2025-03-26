c8ccb042d2ae0ed515c80df5bc16e58a6775465c
julbroersen <julianabroersen2@gmail.com>
2025-03-26 14:21:49 +0100

    added synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Meaning.css";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -7,6 +8,7 @@ export default function Meaning(props) {
       <h3 className="mt-2">{props.meaning.partOfSpeech}</h3>
       <p className="mb-0">{props.meaning.definition}</p>
       <p className="example mt-1 mb-1">{props.meaning.example}</p>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
