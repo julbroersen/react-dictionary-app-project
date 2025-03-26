696181bea2d9f764f98822d9e67a19c9f2fa0dbc
julbroersen <julianabroersen2@gmail.com>
2025-03-26 15:18:32 +0100

    css and bootstrap styling

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,10 +4,10 @@ import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
-    <div className="Meaning mt-4">
-      <h3 className="mt-2">{props.meaning.partOfSpeech}</h3>
-      <p className="mb-0">{props.meaning.definition}</p>
-      <p className="example mt-1 mb-1">{props.meaning.example}</p>
+    <div className="Meaning mt-5 me-2">
+      <h3 className="mt-2 mb-1">{props.meaning.partOfSpeech}</h3>
+      <p className="mb-1">{props.meaning.definition}</p>
+      <p className="example mb-0">{props.meaning.example}</p>
       <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,7 +4,7 @@ import "./Synonyms.css";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <ul className="Synonyms">
+      <ul className="Synonyms ps-0 mt-0 pt-0 text-start">
         {props.synonyms.map(function (synonym, index) {
           return <li key={index}>{synonym}</li>;
         })}
